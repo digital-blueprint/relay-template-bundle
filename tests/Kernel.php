85c88beb32437f3bea90dd33994f2997debfb4ac
Christoph Reiter <reiter.christoph@gmail.com>
2024-04-18 11:45:35 +0200

    Add support for api-platform 3.2

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -48,9 +48,5 @@ class Kernel extends BaseKernel
         ]);
 
         $container->extension('dbp_relay_template', []);
-
-        $container->extension('api_platform', [
-            'metadata_backward_compatibility_layer' => false,
-        ]);
     }
 }
